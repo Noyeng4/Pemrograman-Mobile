43e7c10be6f92ea90857d77e10c8f3cea0968cc0
Noyeng4 <115410703+Noyeng4@users.noreply.github.com>
2024-06-23 03:22:03 +0800

    Add files via upload

---
+++ Modul 3/java/com/example/android/unscramble/ui/game/ListofWords.kt
@@ -0,0 +1,16 @@
+package com.example.android.unscramble.ui.game
+
+
+const val MAX_NO_OF_WORDS = 10
+const val SCORE_INCREASE = 20
+
+// List with all the words for the Game
+val allWordsList: List<String> =
+    listOf("alucard", "argus", "aurora", "balmond",
+        "bane", "barats", "belerick", "benedetta",
+        "chou", "claude", "diggie", "esmeralda",
+        "fanny", "gusion", "granger", "guinevere",
+        "hanabi", "harith", "hayabusa", "helcurt",
+        "hilda", "jawhead", "kagura", "khaleed",
+        "khufra", "kimmy", "lapu-lapu", "leomord",
+        "lesley", "ling", "moskov", "miya")
